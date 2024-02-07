7c1d6c22d1546742224729e108802b7e0d987859
Dheraj M <135042315+dherajm@users.noreply.github.com>
2024-02-07 08:07:31 +0530

    Add files via upload

---
+++ Intermidiate/main.py
@@ -0,0 +1,51 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+# import random
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake-e")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend_segment()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 299 or snake.head.xcor() < -299 or snake.head.ycor() > 299 or snake.head.ycor() < -299:
+        scoreboard.reset()
+        snake.reset_snake()
+
+    # Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset_snake()
+
+screen.exitonclick()
