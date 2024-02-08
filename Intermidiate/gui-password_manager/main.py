04b37bae2780c500d2cb8b4dc92ebf42f75a6a6a
Dheraj M <135042315+dherajm@users.noreply.github.com>
2024-02-08 17:52:22 +0530

    Update main.py

--- Intermidiate/gui-password_manager/main.py
+++ Intermidiate/gui-password_manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 import tkinter.messagebox as mb
 import random
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -33,18 +34,54 @@ def add_credentials():
     email_text = email_entry.get()
     password_text = password_entry.get()
 
+    final_data = {
+        website_text: {
+            'email': email_text,
+            'password': password_text
+        }
+    }
+
     if not website_text or not email_text or not password_text:
         mb.showerror("Fill in all fields", "Fill in all the fields")
     else:
         is_ok = mb.askyesno(title=website_text, message=f"Credentials: \nWebsite: {website_text} \n"
-                                                        f"Email: {email_text} \nPassword: {password_text} \n\nSave Credentials?")
+                                                        f"Email: {email_text} \nPassword: {password_text} \n\nSave "
+                                                        f"Credentials?")
 
         if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"\n{website_text} | {email_text} | {password_text}\n")
+            try:
+                with open("data.json", "r") as file:
+                    data = json.load(file)
+            except (FileNotFoundError, json.decoder.JSONDecodeError):
+                data = {}
+
+            data.update(final_data)
+
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
+
                 website_entry.delete(0, END)
                 email_entry.delete(0, END)
                 password_entry.delete(0, END)
+
+
+# ---------------------------- SEARCH CREDENTIALS ------------------------------- #
+def search_credentials():
+    website_text = website_entry.get()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+            credentials = data.get(website_text)
+
+            if credentials:
+                mb.showinfo("Credentials Found", f"Website: {website_text}\nEmail: {credentials['email']}\n"
+                                                 f"Password: {credentials['password']}")
+            else:
+                mb.showinfo("Credentials Not Found", f"No credentials found for {website_text}")
+    except FileNotFoundError:
+        mb.showerror("File not found!")
+    except json.decoder.JSONDecodeError:
+        mb.showerror("Invalid Data! Error decoding the JSON")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -68,8 +105,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=38)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=38)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -77,6 +114,8 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", command=search_credentials, width=13)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=add_credentials)
