5f4e73e39854fbc422f39c22164daffba7012425
Dheraj M <135042315+dherajm@users.noreply.github.com>
2024-02-09 21:43:07 +0530

    Update __init__.py

--- Intermidiate +/automated-birthday-wisher/__init__.py
+++ Intermidiate +/automated-birthday-wisher/__init__.py
@@ -5,8 +5,8 @@ import pandas
 now = dt.datetime.today()
 today = (now.month, now.day)
 
-my_email = "codegeek3813@gmail.com"
-app_password = "nmwnvbeajzhyvowa"
+my_email = "<EMAIL>"                   #Recomended not to use your personal or work email IDs.
+app_password = "<APP-PASSWORD>"        #To learn how to obtain an app password for your gamil, watch a youtube video or do some googling.
 
 data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["Month"], data_row["Day"]): data_row for (index, data_row) in data.iterrows()}
@@ -15,12 +15,12 @@ if today in birthdays_dict:
 
     birthday_person = birthdays_dict[today]
     name = birthday_person["Name"]
+    reciever_email = birthday_person["Email"]
 
     with smtplib.SMTP('smtp.gmail.com') as connection:
-        # Secure the email and encrypt the data
         connection.starttls()
         connection.login(user=my_email, password=app_password)
 
         connection.sendmail(from_addr=my_email,
-                            to_addrs="mdheraj0403@gmail.com",
-                            msg=f"Subject: Happy Birthday!\n\nDear {name},\n\nWishing you a day filled with joy, laughter, and all the things that make you happiest!\nCheers to another amazing year ahead. Happy birthday!\n\nBest Regards from,\nDheraj M")
+                            to_addrs=reciever_email,
+                            msg=f"Subject: Happy Birthday!\n\nDear {name},\n\nWishing you a day filled with joy, laughter, and all the things that make you happiest!\nCheers to another amazing year ahead. Happy birthday!\n\nBest Regards from,\n[Your Name]")
